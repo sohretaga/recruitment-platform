ac6f6fb1f6b9f665ac2c9b86f7dab70f51fd19db
Sohret Aga <shohretaga@gmail.com>
2023-12-25 18:14:04 +0400

    created job list

--- recruitment_main/urls.py
+++ recruitment_main/urls.py
@@ -4,5 +4,6 @@ from recruitment_main.views import main
 app_name = 'main'
 
 urlpatterns = [
-    path('', main.index, name='main-index')
+    path('', main.index, name='main-index'),
+    path('vacancy/load', main.vacancy_load, name='vacancy-load'),
 ]
--- recruitment_main/views/main.py
+++ recruitment_main/views/main.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from recruitment_cp.models import ParameterCareerType, ParameterCareerLevel, ParameterLocation, ParameterFTE, ParameterEmployeeType
+from recruitment_cp.models import ParameterCareerType, ParameterCareerLevel, ParameterLocation, ParameterFTE, ParameterEmployeeType, ParameterVacancy
 
 # Create your views here.
 
@@ -9,6 +9,7 @@ def index(request):
     locations = ParameterLocation.objects.all()
     fte = ParameterFTE.objects.all()
     employment_types = ParameterEmployeeType.objects.all()
+    vacancies = ParameterVacancy.objects.all()
 
     context = {
         'career_types': career_types,
@@ -16,6 +17,10 @@ def index(request):
         'locations': locations,
         'fte': fte,
         'employment_types': employment_types,
+        'vacancies': vacancies,
     }
 
     return render(request, 'main/index.html', context)
+
+def vacancy_load(request):
+    pass
