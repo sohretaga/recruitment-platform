7ef2edc90312b7ee459de0e088c64bd90a853dea
Sohret Aga <shohretaga@gmail.com>
2023-12-25 16:40:53 +0400

    main page edited

--- recruitment_main/views/main.py
+++ recruitment_main/views/main.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from recruitment_cp.models import ParameterCareerType, ParameterCareerLevel, ParameterLocation, ParameterFTE, ParameterEmployeeType
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    career_types = ParameterCareerType.objects.all()
+    career_levles = ParameterCareerLevel.objects.all()
+    locations = ParameterLocation.objects.all()
+    fte = ParameterFTE.objects.all()
+    employment_types = ParameterEmployeeType.objects.all()
+
+    context = {
+        'career_types': career_types,
+        'career_levels': career_levles,
+        'locations': locations,
+        'fte': fte,
+        'employment_types': employment_types,
+    }
+
+    return render(request, 'main/index.html', context)
