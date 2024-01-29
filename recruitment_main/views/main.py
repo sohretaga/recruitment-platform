40e8596d78a351e132d4583359be0b59d074534c
Sohret Aga <shohretaga@gmail.com>
2024-01-30 01:35:09 +0400

    January 30, 01:35

--- recruitment_main/views/main.py
+++ recruitment_main/views/main.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from recruitment_cp.models import ParameterCareerType, ParameterCareerLevel, ParameterLocation, ParameterFTE, ParameterEmployeeType, ParameterVacancy
+from django.core import serializers
+
+from ast import literal_eval
 
 # Create your views here.
 
@@ -23,4 +27,32 @@ def index(request):
     return render(request, 'main/index.html', context)
 
 def vacancy_load(request):
-    pass
+    career_type = request.POST.get('career_type')
+    career_level = request.POST.get('career_level')
+    location = request.POST.get('location')
+    fte = request.POST.get('fte')
+    employee_type = literal_eval(request.POST.get('employee_type'))
+
+    filters = {}
+
+    if career_type != '---':
+        filters['career_type'] = career_type
+    
+    if career_level != '---':
+        filters['career_level'] = career_level
+
+    if location != '---':
+        filters['location'] = location
+
+    if fte != '---':
+        filters['fte'] = fte
+
+    vacancies = ParameterVacancy.objects.filter(**filters, employee_type__in = employee_type)
+
+    vacancies_json = serializers.serialize('json', vacancies)
+
+    context = {
+        'vacancies': vacancies_json
+    }
+
+    return JsonResponse(context)
