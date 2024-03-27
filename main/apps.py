07aa02e095cc5a446d8e734ab3e4b1bcece52e8b
Sohret Aga <shohretaga@gmail.com>
2024-03-27 18:20:36 +0400

    March 27, 18:20

--- main/apps.py
+++ main/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RecruitmentMainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recruitment_main'
+    name = 'main'
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from recruitment_main import views
+from main import views
 
 app_name = 'main'
 
