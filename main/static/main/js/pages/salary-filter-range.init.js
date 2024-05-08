f9c24c7912fef1e0bef26962b081b56290b8cc45
Sohret Aga <shohretaga@gmail.com>
2024-05-08 19:18:32 +0400

    May 08, 19:18

---
+++ main/static/main/js/pages/salary-filter-range.init.js
@@ -0,0 +1,1 @@
+var slider1=document.getElementById("slider1");noUiSlider.create(slider1,{start:[0],step:1,range:{min:[0],max:[1000]}});var slider1Value=document.getElementById("slider1-span");slider1.noUiSlider.on("update",function(e,r){slider1Value.innerHTML=e[r]});
