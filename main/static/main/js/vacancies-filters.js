43e88b9d2b9a6ed8833f98b7c3d03e425bf81c85
Sohret Aga <shohretaga@gmail.com>
2024-05-26 04:53:49 +0400

    May 26, 04:53

--- main/static/main/js/vacancies-filters.js
+++ main/static/main/js/vacancies-filters.js
@@ -15,6 +15,24 @@ $.ajaxSetup({
     }
 });
 
+var slider1 = document.getElementById("slider1");
+
+noUiSlider.create(slider1, {
+    start: [1000, 10000],
+    step: 1,
+    connect: true,
+    range: {
+        min: [0],
+        max: [10000]
+    },
+});
+
+var slider1Value = document.getElementById("slider1-span");
+
+slider1.noUiSlider.on("update", function(values, handle) {
+    slider1Value.innerHTML = `${values[0]} - ${values[1]}`;
+});
+
 class DataCollector {
 
     getSalaryRangeValue() {
