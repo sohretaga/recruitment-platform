23fdd142cff9fa580648460d8ed4bb98fd3f98ae
Sohret Aga <shohretaga@gmail.com>
2024-06-15 02:03:25 +0400

    June 15, 02:03

--- main/views.py
+++ main/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
 from job.utils import fetch_vacancies
 from user.models import Employer
+from job.models import Vacancy
 
+import math
 # Create your views here.
 
 def index(request):
     vacancies = fetch_vacancies(request)
-    sliders = Employer.objects.filter(slider=True, user__profile_photo__isnull=False).exclude(user__profile_photo='')
+    company_slider = Employer.objects.filter(slider=True, user__profile_photo__isnull=False).exclude(user__profile_photo='')
+
+
+    def get_objects_in_sublists(objects_per_list=5):
+        objects = list(Vacancy.objects.all().order_by('?')[:10])
+        
+        total_objects = len(objects)
+        
+        number_of_lists = math.ceil(total_objects / objects_per_list)
+        
+        sublists = [
+            objects[i * objects_per_list:(i + 1) * objects_per_list]
+            for i in range(number_of_lists)
+        ]
+    
+        return sublists
+    
     
     context = {
         **vacancies,
-        'sliders': sliders
+        'company_slider': company_slider,
+        'highlight_slider': get_objects_in_sublists()
     }
 
     return render(request, 'main/index.html', context)
