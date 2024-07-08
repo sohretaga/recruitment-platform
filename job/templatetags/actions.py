82780c1971cb55251b677b19d30259503b206527
Sohret Aga <shohretaga@gmail.com>
2024-07-08 17:11:18 +0400

    July 08, 17:11

--- job/templatetags/actions.py
+++ job/templatetags/actions.py
@@ -9,12 +9,12 @@ def bookmarks(request, vacancy_id) -> str:
         bookmarks_exists = Bookmark.objects.filter(user=request.user, vacancy__id=vacancy_id).exists()
         if bookmarks_exists:
             return 'bookmark-post'
-
     return ''
         
 @register.simple_tag
 def applications(request, vacancy_id) -> bool:
-    if request.user.is_authenticated:
-        return Apply.objects.filter(candidate=request.user.candidate, vacancy__id=vacancy_id).exists()
-    
+    user = request.user
+    if user.is_authenticated and user.user_type == 'candidate':
+        apply_exists = Apply.objects.filter(candidate=request.user.candidate, vacancy__id=vacancy_id).exists()
+        return apply_exists
     return False
