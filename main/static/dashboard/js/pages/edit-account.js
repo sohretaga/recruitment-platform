6f5d789c581199aeba859c912f7acefdeb568bdd
Sohret Aga <shohretaga@gmail.com>
2024-07-18 00:25:12 +0400

    July 18, 00:25

--- main/static/dashboard/js/pages/edit-account.js
+++ main/static/dashboard/js/pages/edit-account.js
@@ -1,9 +1,13 @@
-const profileImg = document.getElementById('profile-img');
 const previewImg = (input) => {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
-            document.getElementById('profile-img').src = e.target.result;
+            if (input.name == 'profile_photo') {
+                document.getElementById('profile-img').src = e.target.result;
+            }
+            else if(input.name == 'background_image') {
+                document.getElementById('background-img').src = e.target.result;
+            };
         };
         reader.readAsDataURL(input.files[0]);
     };
