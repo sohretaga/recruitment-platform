0bc05d240c77cdcbf82f119f7ff959eacf6df349
Sohret Aga <shohretaga@gmail.com>
2024-07-29 17:21:59 +0400

    July 29, 17:21

---
+++ main/static/main/js/ajax/candidate-list.js
@@ -0,0 +1,4 @@
+const choicesSingleLocation = new Choices('#choices-single-location');
+const choicesSingleCategories = new Choices('#choices-single-categories');
+const choicesSingleFilterOrderby = new Choices('#choices-single-filter-orderby');
+const choicesCandidatePage = new Choices('#choices-candidate-page');
