0c143b5e8b829b2a4a6c8b8d6c8c17a48bdc4f14
Sohret Aga <shohretaga@gmail.com>
2024-07-29 19:24:17 +0400

    July 29, 19:24

--- user/admin.py
+++ user/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from .models import CustomUser, Candidate, Employer
 
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['get_full_name', 'username', 'user_type']
+
+    def get_full_name(self, obj):
+        if obj.user_type == 'employer':
+            return obj.first_name
+        else:
+            return f'{obj.first_name} {obj.last_name}'
+
+    get_full_name.short_description = 'Full Name'
+
 admin.site.register(Employer)
 admin.site.register(Candidate)
-admin.site.register(CustomUser)
