64dfd2e531cddffd74e99b6e519c2de61d0f873c
Sohret Aga <shohretaga@gmail.com>
2024-08-27 20:41:48 +0400

    August 27, 20:41

--- user/tasks.py
+++ user/tasks.py
@@ -1,5 +1,7 @@
+from django.db.models import Q
 from celery import shared_task
 from job.models import Vacancy
+from user.models import Candidate
 
 import json
 import websocket
@@ -9,10 +11,10 @@ def send_notification_to_preferred_candidates(vacancy_id, ws_protocol, host):
     vacancy = Vacancy.objects.get(id=vacancy_id)
 
     try:
-        employer = vacancy.employer.preferred_employers.values_list('candidate__user__id', flat=True)
-        career_type = vacancy.career_type.preferred_career_types.values_list('candidate__user__id', flat=True)
-        location = vacancy.location.preferred_locations.values_list('candidate__user__id', flat=True)
-        employment_type = vacancy.employment_type.preferred_employment_types.values_list('candidate__user__id', flat=True)
+        employer = vacancy.employer.preferred_employers.values_list('candidate', flat=True)
+        career_type = vacancy.career_type.preferred_career_types.values_list('candidate', flat=True)
+        location = vacancy.location.preferred_locations.values_list('candidate', flat=True)
+        employment_type = vacancy.employment_type.preferred_employment_types.values_list('candidate', flat=True)
 
         common_candidates = set(employer).intersection(
             career_type,
@@ -20,16 +22,32 @@ def send_notification_to_preferred_candidates(vacancy_id, ws_protocol, host):
             employment_type
         )
 
-        for user_id in common_candidates:
+        candidates = Candidate.objects.filter(
+            # İlk önce pozisyonel argüman olan Q nesnesini yazıyoruz
+            (
+                Q(preference__min_salary__lte=vacancy.salary) &
+                Q(preference__max_salary__gte=vacancy.salary)
+            ) |
+            (
+                Q(preference__min_salary__lte=vacancy.salary_maximum) &
+                Q(preference__max_salary__gte=vacancy.salary_minimum)
+            ),
+            # Daha sonra keyword argüman olarak id__in koşulunu ekliyoruz
+            id__in=common_candidates
+        ).values_list('user__id', flat=True)
+
+        for target_id in candidates:
+
+            print(target_id)            
 
             text_data = {
                 "content": "PREFERRED",
                 "related_data": vacancy_id,
             }
 
-            target_ws_url = f"{ws_protocol}{host}/ws/send-notifications/{user_id}/"
-            ws = websocket.create_connection(target_ws_url)
-            ws.send(json.dumps(text_data))
-            ws.close()
+            # target_ws_url = f"{ws_protocol}{host}/ws/send-notifications/{user_id}/"
+            # ws = websocket.create_connection(target_ws_url)
+            # ws.send(json.dumps(text_data))
+            # ws.close()
         
     except AttributeError: ...
