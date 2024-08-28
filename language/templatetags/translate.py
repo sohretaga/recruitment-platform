f988239aaee28c6adf2b7d582a690cf5ea6b4942
Sohret Aga <shohretaga@gmail.com>
2024-08-29 03:51:33 +0400

    August 29, 03:51

--- language/templatetags/translate.py
+++ language/templatetags/translate.py
@@ -1,23 +1,8 @@
-
 from django import template
-from django.core.cache import cache
-from language.models import Translation
-from django.conf import settings
+from language.utils import tr as utils_tr
 
 register = template.Library()
 
 @register.simple_tag
 def tr(text: str) -> str:
-    language_code = cache.get('site_language', settings.SITE_LANGUAGE_CODE)
-    params = {
-        'language__code':language_code,
-        'text': text
-    }
-
-    translation_exists = Translation.objects.filter(**params).exists()
-    if translation_exists:
-        translation = Translation.objects.get(**params).translation
-        if translation:
-            return translation
-
-    return text
+    return utils_tr(text)
--- language/utils.py
+++ language/utils.py
@@ -36,10 +36,34 @@ def tr(text: str) -> str:
         'text': text
     }
 
-    translation_exists = Translation.objects.filter(**params).exists()
-    if translation_exists:
+    try:
         translation = Translation.objects.get(**params).translation
         if translation:
             return translation
+    except Translation.DoesNotExist:
+        pass
 
     return text
+
+
+
+
+# import hashlib
+
+# def tr(text: str) -> str:
+#     language_code = cache.get('site_language', settings.SITE_LANGUAGE_CODE)
+    
+#     text_hash = hashlib.md5(text.encode('utf-8')).hexdigest()
+#     cache_key = f'translation_{language_code}_{text_hash}'
+    
+#     translation = cache.get(cache_key)
+#     if translation is None:
+#         try:
+#             translation = Translation.objects.get(
+#                 language__code=language_code, text=text
+#             ).translation
+#             cache.set(cache_key, translation)
+#         except Translation.DoesNotExist:
+#             translation = text
+
+#     return translation
