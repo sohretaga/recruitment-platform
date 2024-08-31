126cb05c072fbca956b61d07f1fc9973cebdc7d0
Sohret Aga <shohretaga@gmail.com>
2024-08-31 04:40:48 +0400

    August 31, 04:40

--- recruitment_cp/templatetags/site_content.py
+++ recruitment_cp/templatetags/site_content.py
@@ -9,17 +9,40 @@ register = template.Library()
 
 @register.simple_tag
 def get_site_content(key):
-    language = cache.get('site_language', settings.SITE_LANGUAGE_CODE)
-    title = cache.get(get_cache_key(f'title_{key}', language))
-    content = cache.get(get_cache_key(f'content_{key}', language))
-    image = cache.get(get_cache_key(f'image_{key}', ''))
+    try:
+        language = cache.get('site_language', settings.SITE_LANGUAGE_CODE)
 
-    content = {
-        'title': title,
-        'content': content
-    }
+        title_key = get_cache_key(f'title_{key}', language)
+        content_key = get_cache_key(f'content_{key}', language)
+        image_key = get_cache_key(f'image_{key}', '')
 
-    if image:
-        content['image'] = image
-    
-    return content
+        title = cache.get(title_key)
+        content = cache.get(content_key)
+        image = cache.get(image_key)
+
+        if not title or not content:
+            site_content = SiteContent.objects.annotate(
+                title=F(f'title_{language}'),
+                content=F(f'content_{language}'),
+            ).values('title', 'content', 'image').get(page=key)
+
+            title = site_content['title']
+            content = site_content['content']
+            image = site_content.get('image', '')
+
+            cache.set(title_key, title, None)
+            cache.set(content_key, content, None)
+            cache.set(image_key, image, None)
+
+        content_dict = {
+            'title': title,
+            'content': content
+        }
+
+        if image:
+            content_dict['image'] = image
+
+        return content_dict
+
+    except SiteContent.DoesNotExist:
+        return ''
