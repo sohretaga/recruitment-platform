6cc9da2ff84445aa70028f2e7a5bcc269469ade5
Sohret Aga <shohretaga@gmail.com>
2024-12-07 21:28:14 +0400

    December 07, 21:28

--- language/views.py
+++ language/views.py
@@ -13,15 +13,12 @@ def set_language(request):
     language_code = data.get('language', settings.SITE_LANGUAGE_CODE)
 
     if request.user.is_authenticated:
-        cache.delete(f'site_language_{request.user.username}')
         cache.set(f'site_language_{request.user.username}', language_code, timeout=31536000) # 31536000 = 1 year
-        cache.delete(f'site_language_{request.session.session_key}')
         return JsonResponse({'process': 'username'})
 
     else:
         if not request.session.session_key:
             request.session.create()
-        cache.delete(f'site_language_{request.session.session_key}')
+
         cache.set(f'site_language_{request.session.session_key}', language_code, timeout=604800) # 604800 = 1 week
-        cache.delete(f'site_language_{request.user.username}')
         return JsonResponse({'process': 'session'})
