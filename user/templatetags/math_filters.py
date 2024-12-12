eb9f7c3db6ad7093005f0603af98cc3c29ba680e
Sohret Aga <shohretaga@gmail.com>
2024-12-12 19:09:02 +0400

    December 12, 19:09

---
+++ user/templatetags/math_filters.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def subtract(value, arg):
+    try:
+        return int(value) - int(arg)
+    except (ValueError, TypeError):
+        return ''
